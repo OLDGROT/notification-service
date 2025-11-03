8d973248266d79ce5170957b8cb529224709cf64
OLDGROT <sashawhite10596@gmail.com>
2025-11-03 07:13:20 +0300

    discover Service

--- src/main/java/org/example/notificationservice/controller/NotificationController.java
+++ src/main/java/org/example/notificationservice/controller/NotificationController.java
@@ -1,15 +1,12 @@
 package org.example.notificationservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.notificationservice.exception.UnknownTypeException;
 import org.example.notificationservice.service.EmailService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/notifications")
+@RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
     private final EmailService emailService;
